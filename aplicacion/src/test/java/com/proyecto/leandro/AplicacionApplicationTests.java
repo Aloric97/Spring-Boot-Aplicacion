c182a06e7e07a5a1edb2b1bcde51f10fbbbb7cb9
Leandro Tombetta <57770761+Aloric97@users.noreply.github.com>
2021-10-20 01:14:59 -0300

    Comit inicial con eclipse

---
+++ aplicacion/src/test/java/com/proyecto/leandro/AplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.leandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
